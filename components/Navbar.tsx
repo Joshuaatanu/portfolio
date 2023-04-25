5331c0812bbba21e3781ffd22cfe70a73353ce5a
Joshua Atanu <atanu.joshua@gmail.com>
2023-04-25 08:29:59 +0100

    finished the nav and hero

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,23 +7,21 @@ export default function Navbar() {
   return (
     <div className="">
       <nav className=" z-40 w-full pt-6 ">
-        <div className="px-3 md:px-16 py-3 rounded-full  flex flex-row items-center h-fit bg-opacity-50 pt-4 mx-[35%] bg-slate-100  duration-500">
+        <div className="px-3 md:px-16 py-3 rounded-full w-[1026px]  flex flex-row items-center h-fit bg-opacity-50 pt-4 mx-[25%] bg-slate-100  duration-500">
           <a href="home">
             <img src="Ja.svg" className="" alt="logo" />
           </a>
-          <div className="flex-row ml-4 gap-7 space-x-4 border-0">
-            <Link href="about" className="text-headline">
-              ABOUT ME
-            </Link>
-            <Link href="#">MY WORKS</Link>
-            <Link href="#">CONTACT ME</Link>
+          <div className="flex-row ml-[20%] gap-7 space-x-28 border-0">
+            <Link href="about"   className=" text-zinc-600 hover:text-gray-300 transition"> ABOUT ME</Link>
+            <Link href="#"  className=" text-zinc-600 hover:text-gray-300 transition">MY WORKS</Link>
+            <Link href="#"  className=" text-zinc-600 hover:text-gray-300 transition">CONTACT ME</Link>
           </div>
           <img
             src="toggle-lite.svg"
             height="26.5px"
             width="52.5px"
             alt=""
-            className=""
+            className="ml-[13%]"
           />
         </div>
       </nav>
