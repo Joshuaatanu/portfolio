04d6f9ed14cc56690073c65ae98c1f5545394768
Joshua Atanu <atanu.joshua@gmail.com>
2023-09-17 11:07:17 +0100

    removed home

--- pages/home.tsx
+++
@@ -1,9 +0,0 @@
-// /* eslint-disable react/no-unescaped-entities */
-// import Navbar from "@/components/Navbar";
-// import React from "react";
-
-// export default function home() {
-//   return (
-  
-//   );
-// }
